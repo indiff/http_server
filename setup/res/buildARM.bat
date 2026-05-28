1fe319e03f57798e3c12301267038441c613c45d
David Carpenter <github@voidtools.com>
2026-05-28 14:39:27 +0930

    added ARM/ARM64

---
+++ setup/res/buildARM.bat
@@ -0,0 +1,2 @@
+del "C:\dev\http_server\setup\res\setup.dll.bz2"
+"C:\Program Files\7-Zip\7z.exe" a -tbzip2 C:\dev\http_server\setup\res\setup.dll.bz2 "C:\dev\http_server\vs2019\ARM\Release\http_serverARM.dll" -mx=9
---
+++ setup/res/buildARM64.bat
@@ -0,0 +1,2 @@
+del "C:\dev\http_server\setup\res\setup.dll.bz2"
+"C:\Program Files\7-Zip\7z.exe" a -tbzip2 C:\dev\http_server\setup\res\setup.dll.bz2 "C:\dev\http_server\vs2019\ARM64\Release\http_serverARM64.dll" -mx=9
