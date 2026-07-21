7c929aea3a96ebeda3244fc0a270abc4a9f995e7
indiff <indiff@126.com>
2026-07-21 13:32:32 +0800

    fix compile

--- setup/res/build64.bat
+++ setup/res/build64.bat
@@ -1,2 +1,4 @@
+rem del "C:\dev\everything\plugins\http_server\setup\res\setup.dll.bz2"
+rem "C:\Program Files\7-Zip\7z.exe" a -tbzip2 C:\dev\everything\plugins\http_server\setup\res\setup.dll.bz2 "C:\dev\everything\plugins\http_server\x64\Release\http_server64.dll" -mx=9
 del "C:\dev\everything\plugins\http_server\setup\res\setup.dll.bz2"
-"C:\Program Files\7-Zip\7z.exe" a -tbzip2 C:\dev\everything\plugins\http_server\setup\res\setup.dll.bz2 "C:\dev\everything\plugins\http_server\x64\Release\http_server64.dll" -mx=9
+7z.exe a -tbzip2 C:\dev\everything\plugins\http_server\setup\res\setup.dll.bz2 "C:\dev\everything\plugins\http_server\x64\Release\http_server64.dll" -mx=9
